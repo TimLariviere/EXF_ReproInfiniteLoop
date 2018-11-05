193863a97f62fe9cc1caba43166ba20f2dbcd2ee
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-11-05 22:15:46 +0100

    Workaround v2

--- EXF_ReproInfiniteLoop/EXF_ReproInfiniteLoop.fs
+++ EXF_ReproInfiniteLoop/EXF_ReproInfiniteLoop.fs
@@ -4,6 +4,8 @@ namespace EXF_ReproInfiniteLoop
 open Elmish.XamarinForms
 open Elmish.XamarinForms.DynamicViews
 open Xamarin.Forms
+open System.Threading
+open System.Collections.Generic
 
 module Workaround =
     let throttle fn timeout =
@@ -23,24 +25,60 @@ module Workaround =
         )
         mailbox.Post
 
+module WorkaroundV2 =
+    type internal Memoizations() =
+        static let t = Dictionary<obj, CancellationTokenSource>(HashIdentity.Structural)
+        static member T = t
+        static member Add(key: obj, cts: CancellationTokenSource) =
+            Memoizations.T.[key] <- cts
+        static member Remove(key: obj) =
+            Memoizations.T.Remove key |> ignore
+
+    let debounce (timeout: int) fn value =
+        let key = fn.GetType()
+
+        // Cancel previous debouncer
+        match Memoizations.T.TryGetValue(key) with
+        | true, cts -> cts.Cancel()
+        | _ -> ()
+
+        // Create a new cancellation token and memoize it
+        let cts = new CancellationTokenSource()
+        Memoizations.Add(key, cts)
+
+        // Start a new debouncer
+        (async {
+            try
+                do! Async.Sleep timeout
+                Memoizations.Remove(key)
+                fn value
+            with
+            | _ -> ()
+        })
+        |> (fun a -> Async.StartImmediate(a, cts.Token))
+
 module App = 
     type Model = 
-      { 
-        Text : string
+      { Text : string
         TextWorkaround: string
-      }
+        TextWorkaroundV2: string }
 
-    type Msg = | TextChanged of string | TextChangedWorkaround of string
+    type Msg =
+        | TextChanged of string
+        | TextChangedWorkaround of string
+        | TextChangedWorkaroundV2 of string
 
-    let init () = { Text = ""; TextWorkaround = "" }, Cmd.none
+    let init () = { Text = ""; TextWorkaround = ""; TextWorkaroundV2 = "" }, Cmd.none
 
     let update msg model =
         match msg with
         | TextChanged text -> { model with Text = text }, Cmd.none
         | TextChangedWorkaround text -> { model with TextWorkaround = text }, Cmd.none
+        | TextChangedWorkaroundV2 text -> { model with TextWorkaroundV2 = text }, Cmd.none
 
     let view (model: Model) dispatch =
         let throttledDispatch = fixf Workaround.throttle dispatch 250
+        let debouncedDispatch = WorkaroundV2.debounce 250 dispatch
 
         View.ContentPage(
           content = View.StackLayout(padding = 20.0,
@@ -53,7 +91,12 @@ module App =
                 View.Label(text="Entry with workaround")
                 View.Entry(text=model.TextWorkaround, verticalOptions = LayoutOptions.CenterAndExpand, textChanged=(fun e ->
                     System.Console.WriteLine("TextChanged Workaround: " + e.NewTextValue)
-                    throttledDispatch (TextChangedWorkaround e.NewTextValue))
+                    debouncedDispatch (TextChangedWorkaround e.NewTextValue))
+                )
+                View.Label(text="Entry with workaround v2")
+                View.Entry(text=model.TextWorkaroundV2, verticalOptions = LayoutOptions.CenterAndExpand, textChanged=(fun e ->
+                    System.Console.WriteLine("TextChanged Workaround v2: " + e.NewTextValue)
+                    debouncedDispatch (TextChangedWorkaroundV2 e.NewTextValue))
                 )
             ]))
 
